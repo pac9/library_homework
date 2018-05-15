82a8231adc3217b2a0417db659e814ef14b0f318
pac9 <pac9@btinternet.com>
2018-05-15 20:00:53 +0100

    add book to borrower's collection

--- src/main/java/Borrower.java
+++ src/main/java/Borrower.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class Borrower {
+
+    private ArrayList<Book> borrowed;
+
+    public Borrower(){
+        this.borrowed = new ArrayList<>();
+    }
+
+    public void borrow(Book stock){
+        this.borrowed.add(stock);
+    }
+
+    public void borrowBooksFromLibrary(Library library) {
+        Book stock = library.removeBook();
+        borrow(stock);
+    }
+
+    public int booksBorrowed(){
+        return this.borrowed.size();
+    }
+
+
+
+
 }
--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,2 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BorrowerTest {
+
+    Borrower borrowed;
+    Book stock;
+    Library library;
+
+    @Before
+    public void before() {
+        borrowed = new Borrower();
+        stock = new Book();
+        library = new Library();
+        library.addBook(stock);
+    }
+
+    @Test
+    public void shelfStartsEmpty() {
+        assertEquals(0, borrowed.booksBorrowed());
+    }
+
+    @Test
+    public void borrowsBook() {
+        borrowed.borrow(stock);
+        assertEquals(1, borrowed.booksBorrowed());
+    }
+
+
+    @Test
+    public void borrowBooksFromLibrary() {
+        borrowed.borrowBooksFromLibrary(library);
+        assertEquals(1, borrowed.booksBorrowed());
+        assertEquals(0, library.bookCount());
+    }
 }
